678be7bb7bba14c240351a8167e55da085c3e797
Rus Inozemtsev <rus.inozemtsev@gmail.com>
2024-05-19 10:21:01 +0300

    fix: package-json-removed-module-and-main

--- __test__/generateId.test.ts
+++ __test__/generateId.test.ts
@@ -6,8 +6,6 @@ it('should generate a unique id', (done) => {
 	done()
 })
 
-// TODO: проверить что они реально сортируются по алфовиту в нужно порядке
-// TODO: перевести на английский комменты
 it('should generate 100 unique ids', (done) => {
 	const ids = new Array(100).fill(0).map(() => Bongo.generateBongoId())
 	const uniqueIds = new Set(ids)
